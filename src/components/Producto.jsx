ca8392d846b7d0a7bf8bb6a9379f0cf385417b03
Pablo Fernandez <pablo.ezequiel.fernandez@gmail.com>
2020-07-03 12:46:03 -0300

    Eventos en React

--- src/components/Producto.jsx
+++ src/components/Producto.jsx
@@ -3,10 +3,18 @@ import React from "react";
 const Producto = ({ producto }) => {
   const { nombre, precio, id } = producto;
 
+  //Agregar producto al carrito
+  const seleccionarProducto = (id) => {
+    console.log("comprando...", id);
+  };
+
   return (
     <div>
       <h2>{nombre}</h2>
       <p>${precio}</p>
+      <button type="button" onClick={() => seleccionarProducto(id)}>
+        Comprar
+      </button>
     </div>
   );
 };
